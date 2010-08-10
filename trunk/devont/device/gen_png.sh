c7eac418a617de18dab6ded83c4857685a06f22b
carueda <carueda@e3600890-0f7f-e2ee-fe08-d0bb47df96a3>
2010-08-10 02:55:22 +0000

    added Platform to 'separate' list

--- trunk/devont/device/gen_png.sh
+++ trunk/devont/device/gen_png.sh
@@ -1,9 +1,17 @@
+# This script generates the PNG for the DevOnt (and other ontologies as
+# convenient).
+
 ont2png=~/bin/ont2png.sh
 
-SEPARATE="http://mmisw.org/ont/mmi/device/{TypedValue,PhysicalProperty,ModelID,Manufacturer}"
+SEPARATE="http://mmisw.org/ont/mmi/device/{TypedValue,PhysicalProperty,ModelID,Manufacturer,Platform}"
 
 ${ont2png} --separate ${SEPARATE} http://mmisw.org/ont/mmi/device
 
 #${ont2png} --separate ${SEPARATE} file:device.owl
 
+#${ont2png} --separate ${SEPARATE} file:device2.owl
+
+#${ont2png} http://sweet.jpl.nasa.gov/1.1/substance.owl
+#${ont2png} http://www.qudt.org/qudt/owl/1.0.0/qudt.owl
+#${ont2png} http://www.w3.org/2007/ont/unit
 
